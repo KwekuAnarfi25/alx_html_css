062d68f4692a54bb318a3d75678d52dab1ecdbe1
Jedidiah Anarfi <janarfi25@gmail.com>
2025-10-12 21:26:24 +0100

    8-humberger menu

---
+++ headphones/8-script.js
@@ -0,0 +1,8 @@
+
+  const menuBtn = document.getElementById('menu-btn');
+  const navLinks = document.getElementById('nav-links');
+
+  menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('active');
+    navLinks.classList.toggle('active');
+  });
